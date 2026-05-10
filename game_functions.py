bade7c58d665b23d782fdfbb106197f59491c058
Jamie <39521963+JamieWZ@users.noreply.github.com>
2026-05-10 12:30:14 +0800

    added movement/merging, winning/losing conditions

--- game_functions.py
+++ game_functions.py
@@ -27,5 +27,108 @@ def print_board(board):
         print()
     print('-' * 21)
 
+
 def add_new_tile(board):
-    pass
+    add_new = False
+    for row in board:
+        for num in row:
+            if num == 0:
+                add_new = True
+                break
+
+    if add_new:
+        row = random.randint(0, 3)
+        col = random.randint(0, 3)
+        while board[row][col] != 0:
+            row = random.randint(0, 3)
+            col = random.randint(0, 3)
+        board[row][col] = 2
+
+
+def move_up(board):
+    transposed_board = transpose_board(board)
+    move_left(transposed_board)
+    result = transpose_board(transposed_board)
+
+    for i in range(len(result)):
+        for j in range(len(result[i])):
+            board[i][j] = result[i][j]
+
+
+def move_down(board):
+    transposed_board = transpose_board(board)
+    move_right(transposed_board)
+    result = transpose_board(transposed_board)
+
+    for i in range(len(result)):
+        for j in range(len(result[i])):
+            board[i][j] = result[i][j]
+
+
+def move_left(board):
+    # step 1: move zeroes
+    move_zeroes(board)
+
+    # step 2: merge
+    for i in range(len(board)):
+        for j in range(len(board[i]) - 1):
+            if board[i][j] != 0 and board[i][j] == board[i][j + 1]:
+                board[i][j] *= 2
+                board[i][j + 1] = 0
+    move_zeroes(board)
+
+
+def move_right(board):
+    for row in board:
+        row.reverse()
+
+    move_left(board)
+
+    for row in board:
+        row.reverse()
+
+
+def move_zeroes(board):
+    for i in range(len(board)):
+        slow = 0
+        for fast in range(len(board[i])):
+            if board[i][fast] != 0:
+                board[i][slow], board[i][fast] = board[i][fast], board[i][slow] #swap
+                slow += 1
+
+
+def transpose_board(board):
+    rows = len(board)
+    cols = len(board[0])
+
+    new_board = [[0 for i in range(rows)] for j in range(cols)]
+
+    for i in range(rows):
+        for j in range(cols):
+            new_board[j][i] = board[i][j]
+
+    return new_board
+
+
+def is_winner(board):
+    for row in board:
+        for num in row:
+            if num == 2048:
+                return True
+    return False
+
+
+def is_game_over(board):
+    row = len(board)
+    col = len(board[0])
+
+    for i in range(row):
+        for j in range(col):
+            if board[i][j] == 0:
+                return False
+            if i < row - 1 and board[i][j] == board[i + 1][j]:
+                return False
+            if j < col - 1 and board[i][j] == board[i][j + 1]:
+                return False
+
+    return True
--- main.py
+++ main.py
@@ -3,7 +3,46 @@ from game_functions import *
 
 def main():
     board = create_board()
-    print_board(board)
+    play_game(board)
+
+def play_game(board):
+    continue_game = True
+    while continue_game:
+        print_board(board)
+
+        user_input = input("Enter your movement:w/a/s/d ")
+        if user_input == "w":
+            move_up(board)
+        elif user_input == "a":
+            move_left(board)
+        elif user_input == "s":
+            move_down(board)
+        elif user_input == "d":
+            move_right(board)
+        else:
+            print("Invalid input, please try again")
+            continue
+
+        add_new_tile(board)
+
+        if is_winner(board):
+            print_board(board)
+            print("You win!")
+            user_choice = input("Would you like to play again? (y/n) ")
+            if user_choice == "y":
+                play_game(create_board())
+            elif user_choice == "n":
+                return
+
+        if is_game_over(board):
+            print('Game Over! You lose!')
+            user_choice = input("Would you like to play again? (y/n) ")
+            if user_choice == "y":
+                play_game(create_board())
+            elif user_choice == "n":
+                return
+
+
 
 if __name__ == '__main__':
     main()
